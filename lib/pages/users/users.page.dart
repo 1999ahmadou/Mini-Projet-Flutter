cd2fa38dc0837f99c9f50dced31bf730097e7286
ahmadoukassoum <ahmadoukassoum7@gmail.com>
2021-04-04 04:21:20 +0100

    affichage des données dans une listView

--- lib/pages/users/users.page.dart
+++ lib/pages/users/users.page.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http; // dependence pour envoyer les requettes
 
 class UsersPage extends StatefulWidget {
   @override
@@ -9,8 +11,22 @@ class UsersPage extends StatefulWidget {
 class _UsersPageState extends State<UsersPage> {
   String query;
   bool notVisible=false;
-
   TextEditingController queryTextEditingController=new TextEditingController();
+  dynamic data; // pour stocker les données au format json
+
+  void _search(String query) { //envoie une requette http vers la partie back-end (api de github) et on récupére les données
+    //String url="https://api.github.com/search/users?q=${query}&per_page=20&page=0";
+    http.get(Uri.parse('https://api.github.com/search/users?q=${query}&per_page=20&page=0'))
+        .then((response){
+          setState(() {
+            this.data=json.decode(response.body);
+          });
+    })
+        .catchError((err){
+          print(err);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,9 +75,37 @@ class _UsersPageState extends State<UsersPage> {
                 IconButton(icon: Icon(Icons.search,color: Colors.deepOrange,),onPressed: (){
                   setState(() {
                     this.query=queryTextEditingController.text; // récuperation du text qui à été saisit
+                    _search(query);
                   });
                 },)
               ],
+            ),
+            Expanded(
+              child: ListView.builder( //permet de faire une boucle et d'afficher les elements
+                itemCount:(data==null)?0: data['items'].length,  // nombre d'element dans la liste
+                  itemBuilder: (context,index){
+                    return ListTile(  // Element de la liste
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: NetworkImage(data['items'][index]['avatar_url']),
+                                radius: 40,
+                              ),
+                              SizedBox(width: 20,),
+                              Text("${data['items'][index]['login']}"),
+                            ],
+                          ),
+                          CircleAvatar(
+                            child: Text("${data['items'][index]['score']}"),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+              ),
             )
           ],
         ),
